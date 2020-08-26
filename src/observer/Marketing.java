0761971992b55697cce6525f9bccef11eb3d96f6
Natdanai <chinirew000@gmail.com>
2020-08-26 21:30:10 +0700

    Update Marketing.java

--- src/observer/Marketing.java
+++ src/observer/Marketing.java
@@ -4,15 +4,16 @@
  */
 package observer;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author sarun
  */
-public class Marketing implements MyObserver {
+public class Marketing implements Observer {
     @Override
-    public void update(Source o) {
-        System.out.println(
-	 "(marketing) headquater is updated to " 
-                + ((HeadQuater)o).getSomeData());
+    public void update(Observable o,Object arg) {
+        System.out.println("(marketing) headquater is updated to "  + ((HeadQuater)o).getSomeData());
     }
 }
